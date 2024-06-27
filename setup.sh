8405d18ec8a033520a47b93405607761dc3f4558
Jeff Valcher <jvalcher@gmail.com>
2024-06-27 01:13:01 -0500

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# deactivate virutual environment with:  $ deactivate
-
-printf "\nInstalling requirements... "
+# Set up virtual environment and install packages
 
 python -m venv venv
 source ./venv/bin/activate
-
-pip install -r requirements.txt > /dev/null 2>&1
-
-printf "done\n\n"
-
+pip install -r requirements.txt
