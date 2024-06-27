de2c6ce90d25736f704b2936f6ccfcbad78be4ec
jvalcher <jvalcher@gmail.com>
2024-06-27 10:42:24 -0500

    Adjusted indention, coloring

--- format_gcc_output.py
+++ format_gcc_output.py
@@ -15,13 +15,17 @@ from colors import *
     -------
     See ./colors.py
 '''
-FILE_PATH_COLOR   = B_MAX_GREEN
-LINE_NUM_COLOR    = B_MAX_YELLOW
-MSG_PROMPT_COLOR  = B_MAX_BLUE
-MSG_COLOR         = CYAN
-MSG_STR_COLOR     = YELLOW
-CODE_PROMPT_COLOR = B_MAX_YELLOW
-CARET_COLOR       = BOLD_RED
+ERR_COLOR               = B_MAX_RED
+WARN_COLOR              = B_MAX_YELLOW
+FILE_PATH_COLOR         = B_MAX_GREEN
+LINE_NUM_COLOR          = B_MAX_YELLOW
+MSG_PROMPT_COLOR        = B_MAX_BLUE
+MSG_COLOR               = CYAN
+MSG_STR_COLOR           = YELLOW
+ERR_CODE_PROMPT_COLOR   = ERR_COLOR
+WARN_CODE_PROMPT_COLOR  = WARN_COLOR
+ERR_CARET_COLOR         = ERR_COLOR
+WARN_CARET_COLOR        = WARN_COLOR
 
 '''
     Code line style
@@ -38,26 +42,48 @@ style = 'dracula'
 #style = 'coffee'
 
 
+err_num = 1
+warn_num = 1
+
+
+def create_indent_string (n):
+    indent = ''
+    for _ in range(n):
+        indent = ' ' + indent
+    return indent
+
 
 def print_message (lexer, msg, type_str, file_path, line_number, caret_cols):
 
     term_size = shutil.get_terminal_size()
     term_cols = term_size.columns
-    code_indent = '          '
-    msg_indent  = '          '
+    max_err_dig = 2
+    code_indent = '        '
+    msg_indent  = '        '
+    global err_num
+    global warn_num
 
     # Error  :  <file path>  :  <line number>
     type_str = ''.join([c.upper() if i == 0 else c for i,c in enumerate(type_str)])
     err_buff = ''
+    msg_num = -1
+
     if type_str == 'Error':
-        ERROR_COLOR = B_MAX_RED
-        err_buff = '  '
+        TYPE_COLOR = ERR_COLOR
+        msg_num = err_num
+        err_num += 1
+        err_buff = create_indent_string (len('Warning') - len('Error'))
+        err_buff = create_indent_string ((max_err_dig - len(str(msg_num))) + len(err_buff))
     elif type_str == 'Warning':
-        ERROR_COLOR = B_MAX_YELLOW
-        err_buff = ''
+        TYPE_COLOR = WARN_COLOR
+        msg_num = warn_num
+        warn_num += 1
+        err_buff = create_indent_string (max_err_dig - len(str(msg_num)))
     else:
         ERROR_COLOR = B_MAX_BLUE
-    error = f" {ERROR_COLOR}{type_str}{RESET}: {err_buff}{FILE_PATH_COLOR}{file_path}{RESET}  :  {LINE_NUM_COLOR}{line_number}{RESET}"
+        warn_num = ''
+
+    error = f"{err_buff}{TYPE_COLOR}{type_str} {str(msg_num)}{RESET}:  {FILE_PATH_COLOR}{file_path}{RESET} : {LINE_NUM_COLOR}{line_number}{RESET}"
 
     # message
     prompt = ">>>  "
@@ -84,13 +110,19 @@ def print_message (lexer, msg, type_str, file_path, line_number, caret_cols):
                     code_line = f"Unable to find line number {line_number} in \"{file_path}\""
     except FileNotFoundError:
         code_line = f"Unable to find \"{file_path}\""
+    if type_str == 'Warning':
+        CODE_PROMPT_COLOR = WARN_CODE_PROMPT_COLOR
+    if type_str == 'Error':
+        CODE_PROMPT_COLOR = ERR_CODE_PROMPT_COLOR
     code_line = code_indent + f"{CODE_PROMPT_COLOR}{prompt}{RESET}" + code_line.rstrip('\n')
 
 
     # caret
-    caret_indent = ''
-    for _ in range(caret_cols - stripped_spaces - 2):
-        caret_indent = ' ' + caret_indent
+    if type_str == 'Warning':
+        CARET_COLOR = WARN_CARET_COLOR
+    elif type_str == 'Error':
+        CARET_COLOR = ERR_CARET_COLOR
+    caret_indent = create_indent_string (caret_cols - stripped_spaces - 2)
     if len(caret_indent) == 0:
         code_indent = code_indent[1:]
     caret = code_indent + caret_indent + f'{"": <{len(prompt)}}' + f'{CARET_COLOR}⤴{RESET}'
@@ -110,6 +142,8 @@ def format_gcc_output (lexer, command):
     stdout, stderr = process.communicate()
     output = stdout + stderr
     process.kill()
+    global err_num
+    global warn_num
 
     # convert GCC json to dict
     i = output.find("[{")
@@ -123,6 +157,9 @@ def format_gcc_output (lexer, command):
 
     print ("")
     print ("")
+
+    err_num = 1
+    warn_num = 1
 
     # print errors
     for msg in msg_dict:
